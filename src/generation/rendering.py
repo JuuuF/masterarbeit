90e0c72e34e421ca6d581b96f0b7393d713a7e8f
Justin <juswerth@gmx.de>
2024-10-25 14:18:35 +0200

    moved functions into context classes

--- src/generation/rendering.py
+++ src/generation/rendering.py
@@ -37,211 +37,70 @@ bpy.context.scene.render.filepath = OUT_FILE
 # Do stuff
 
 
-def get_geometry_nodes(obj):
-    for modifier in obj.modifiers:
-        if modifier.type == "NODES":
-            return modifier.node_group
-    raise RuntimeError(f"Object {obj} does not contain Geometry Nodes.")
+class SceneUtils:
 
+    def get_geometry_nodes(obj):
+        for modifier in obj.modifiers:
+            if modifier.type == "NODES":
+                return modifier.node_group
+        raise RuntimeError(f"Object {obj} does not contain Geometry Nodes.")
 
-def get_object(name: str):
-    return bpy.data.objects.get(name)
+    def get_object(name: str):
+        return bpy.data.objects.get(name)
 
+    def get_board_radii():
+        board = SceneUtils.get_object("Darts Board")
+        db_geonodes = SceneUtils.get_geometry_nodes(board)
+        radii = []
+        for i in range(1, 7):
+            r = db_geonodes.interface.items_tree.get(f"Radius {i}").default_value
+            radii.append(r)
+        return radii
 
-def get_board_radii():
-    board = get_object("Darts Board")
-    db_geonodes = get_geometry_nodes(board)
-    radii = []
-    for i in range(1, 7):
-        r = db_geonodes.interface.items_tree.get(f"Radius {i}").default_value
-        radii.append(r)
-    return radii
+    def calculate_dart_score():
 
+        radii = SceneUtils.get_board_radii()
+        numbers = [6,13,4,18,1,20,5,12,9,14,11,8,16,7,19,3,17,2,15,10]  # fmt: skip
 
-def calculate_dart_score():
+        def get_dart_score(i):
+            dart = SceneUtils.get_object(f"Dart {i}")
+            board = SceneUtils.get_object("Darts Board")
+            x = dart.location[0] - board.location[0]
+            y = dart.location[2] - board.location[2]
 
-    radii = get_board_radii()
-    numbers = [6, 13, 4, 18, 1, 20, 5, 12, 9, 14, 11, 8, 16, 7, 19, 3, 17, 2, 15, 10]
+            r = np.sqrt(x**2 + y**2)
+            theta = np.degrees(np.arctan2(y, x))
 
-    def get_dart_score(i):
-        dart = get_object(f"Dart {i}")
-        board = get_object("Darts Board")
-        x = dart.location[0] - board.location[0]
-        y = dart.location[2] - board.location[2]
+            number_idx = round(theta / (360 / 20))
+            number = numbers[number_idx]
+            score_str = str(number)
 
-        r = np.sqrt(x**2 + y**2)
-        theta = np.degrees(np.arctan2(y, x))
+            if r < radii[0]:
+                score = 50
+                score_str = "DBull"
+            elif r < radii[1]:
+                score = 25
+                score_str = "Bull"
+            elif r < radii[2]:
+                score = number
+            elif r < radii[3]:
+                score = 3 * number
+                score_str = "T" + score_str
+            elif r < radii[4]:
+                score = number
+            elif r < radii[5]:
+                score = 2 * number
+                score_str = "D" + score_str
+            else:
+                score = 0
+                score_str = "OUT"
 
-        number_idx = round(theta / (360 / 20))
-        number = numbers[number_idx]
-        score_str = str(number)
+            return score, score_str
 
-        if r < radii[0]:
-            score = 50
-            score_str = "DBull"
-        elif r < radii[1]:
-            score = 25
-            score_str = "Bull"
-        elif r < radii[2]:
-            score = number
-        elif r < radii[3]:
-            score = 3 * number
-            score_str = "T" + score_str
-        elif r < radii[4]:
-            score = number
-        elif r < radii[5]:
-            score = 2 * number
-            score_str = "D" + score_str
-        else:
-            score = 0
-            score_str = "OUT"
+        scores = [get_dart_score(i) for i in range(1, 4)]
+        total_score = sum(s[0] for s in scores)
 
-        return score, score_str
-
-    scores = [get_dart_score(i) for i in range(1, 4)]
-    total_score = sum(s[0] for s in scores)
-
-    return scores, total_score
-
-
-def darts_placement():
-
-    def get_random_dart_rotation():
-        drx = np.random.normal(0, 3)
-        dry = np.random.normal(0, 1)
-        drz = np.random.normal(0, 1)
-        return drx, dry, drz
-
-    def get_position_based_dart_rotation(x, y):
-        drx = 0
-        dry = 0
-        drz = 0
-
-        return drx, dry, drz
-
-    def get_random_dart_displacement():
-        r = board_radius * np.sqrt(np.random.random())
-        theta = np.random.random() * 2 * np.pi
-
-        dx = r * np.cos(theta)
-        dy = np.random.normal(0, 0.025)
-        dz = r * np.sin(theta)
-        return dx, dy, dz
-
-    def get_weighted_dart_displacement():
-        weight_map_obj = get_object("Darts Weights")
-        vertex_group = weight_map_obj.vertex_groups[0]
-
-        coordinates = [v.co for v in weight_map_obj.data.vertices]
-        weights = [vertex_group.weight(v.index) for v in weight_map_obj.data.vertices]
-        normalized_weights = np.array(weights) / np.sum(weights)
-
-        coordinate_idx = np.random.choice(
-            list(range(len(coordinates))),
-            # size=1,
-            # replace=True,
-            p=normalized_weights,
-        )
-        return coordinates[coordinate_idx]
-
-    for i in range(1, 4):
-        dart = get_object(f"Dart {i}")
-
-        # Randomly skip dart
-        if i > 1 and True and np.random.random() < 1 / 6:
-            dart.location = (0, 0, 0)
-            continue
-
-        # Location
-        dx, dy, dz = get_weighted_dart_displacement()
-        dart.location = (
-            board_center[0] + dx,
-            board_center[1] + dy,
-            board_center[2] + dz,
-        )
-
-        # Rotation
-        drx, dry, drz = get_position_based_dart_rotation(dx, dz)
-        dart.rotation_euler = (
-            np.radians(-90 + drx),
-            np.radians(dry),
-            np.radians(drz),
-        )
-
-
-def place_camera():
-    cam = get_object("Camera")
-    cam_space = get_object("Camera Space")
-
-    # -------------------------------------------
-    # Position
-    def get_random_cam_position():
-        bbox_min = cam_space.matrix_world @ Vector(
-            cam_space.bound_box[0]
-        )  # lowest corner at 0
-        bbox_max = cam_space.matrix_world @ Vector(
-            cam_space.bound_box[-2]
-        )  # highest corner at -2
-
-        def is_inside(p_w):
-            p_l = cam_space.matrix_world.inverted() @ p_w
-            result, point_on_mesh_l, normal_l, _index = cam_space.closest_point_on_mesh(
-                p_l, distance=10
-            )
-
-            if not result:
-                return False
-
-            # Back to world space
-            point_on_mesh_w = cam_space.matrix_world @ point_on_mesh_l
-            normal_w = cam_space.matrix_world.to_quaternion() @ normal_l
-
-            # Check if point inside
-            p2 = point_on_mesh_w - p_w
-            v = p2.dot(normal_w)
-            return v >= 0
-
-        while True:
-            p = Vector([np.random.uniform(bbox_min[i], bbox_max[i]) for i in range(3)])
-            if is_inside(p):
-                break
-        return p
-
-    cam_pos = get_random_cam_position()
-    cam.location = cam_pos
-
-    # -------------------------------------------
-    # Focus
-    def get_focus_point():
-        board = get_object("Darts Board")
-        board_pos = board.location
-        board_r = (
-            get_geometry_nodes(darts_board)
-            .interface.items_tree.get("Board Diameter")
-            .default_value
-            / 2
-        )
-
-        focus_pos = board_pos.copy()
-        focus_pos[0] += np.random.normal(0, board_r / 3)
-        focus_pos[1] += np.random.normal(0, 0.02)
-        focus_pos[2] += np.random.normal(0, board_r / 3)
-
-        get_object("Camera Focus").location = focus_pos
-
-        return focus_pos
-
-    focus_point = get_focus_point()
-
-    # -------------------------------------------
-    # Rotation
-    def get_cam_rotation(cam_pos, focus_point):
-        view_dir = focus_point - cam_pos
-        rot_quat = view_dir.to_track_quat("-Z", "Y")
-        return rot_quat.to_euler()
-
-    cam_rot = get_cam_rotation(cam_pos, focus_point)
-    cam.rotation_euler = cam_rot
+        return scores, total_score
 
 
 def random_env_texture():
@@ -279,7 +138,152 @@ def random_env_texture():
     bg_node.inputs["Strength"].default_value = np.random.uniform(0.1, 0.8)
 
 
-def render_masks(mask_obj_names: str | list[str]):
+class ObjectPlacement:
+
+    def place_darts():
+
+        def get_random_dart_rotation():
+            drx = np.random.normal(0, 3)
+            dry = np.random.normal(0, 1)
+            drz = np.random.normal(0, 1)
+            return drx, dry, drz
+
+        def get_position_based_dart_rotation(x, y):
+            drx = 0
+            dry = 0
+            drz = 0
+
+            return drx, dry, drz
+
+        def get_random_dart_displacement():
+            r = board_radius * np.sqrt(np.random.random())
+            theta = np.random.random() * 2 * np.pi
+
+            dx = r * np.cos(theta)
+            dy = np.random.normal(0, 0.025)
+            dz = r * np.sin(theta)
+            return dx, dy, dz
+
+        def get_weighted_dart_displacement():
+            weight_map_obj = SceneUtils.get_object("Darts Weights")
+            vertex_group = weight_map_obj.vertex_groups[0]
+
+            coordinates = [v.co for v in weight_map_obj.data.vertices]
+            weights = [
+                vertex_group.weight(v.index) for v in weight_map_obj.data.vertices
+            ]
+            normalized_weights = np.array(weights) / np.sum(weights)
+
+            coordinate_idx = np.random.choice(
+                list(range(len(coordinates))),
+                # size=1,
+                # replace=True,
+                p=normalized_weights,
+            )
+            return coordinates[coordinate_idx]
+
+        for i in range(1, 4):
+            dart = SceneUtils.get_object(f"Dart {i}")
+
+            # Randomly skip dart
+            if i > 1 and True and np.random.random() < 1 / 6:
+                dart.location = (0, 0, 0)
+                continue
+
+            # Location
+            dx, dy, dz = get_weighted_dart_displacement()
+            dart.location = (
+                board_center[0] + dx,
+                board_center[1] + dy,
+                board_center[2] + dz,
+            )
+
+            # Rotation
+            drx, dry, drz = get_position_based_dart_rotation(dx, dz)
+            dart.rotation_euler = (
+                np.radians(-90 + drx),
+                np.radians(dry),
+                np.radians(drz),
+            )
+
+    def place_camera():
+        cam = SceneUtils.get_object("Camera")
+        cam_space = SceneUtils.get_object("Camera Space")
+
+        # -------------------------------------------
+        # Position
+        def get_random_cam_position():
+            bbox_min = cam_space.matrix_world @ Vector(
+                cam_space.bound_box[0]
+            )  # lowest corner at 0
+            bbox_max = cam_space.matrix_world @ Vector(
+                cam_space.bound_box[-2]
+            )  # highest corner at -2
+
+            def is_inside(p_w):
+                p_l = cam_space.matrix_world.inverted() @ p_w
+                result, point_on_mesh_l, normal_l, _index = (
+                    cam_space.closest_point_on_mesh(p_l, distance=10)
+                )
+
+                if not result:
+                    return False
+
+                # Back to world space
+                point_on_mesh_w = cam_space.matrix_world @ point_on_mesh_l
+                normal_w = cam_space.matrix_world.to_quaternion() @ normal_l
+
+                # Check if point inside
+                p2 = point_on_mesh_w - p_w
+                v = p2.dot(normal_w)
+                return v >= 0
+
+            while True:
+                p = Vector(
+                    [np.random.uniform(bbox_min[i], bbox_max[i]) for i in range(3)]
+                )
+                if is_inside(p):
+                    break
+            return p
+
+        cam_pos = get_random_cam_position()
+        cam.location = cam_pos
+
+        # -------------------------------------------
+        # Focus
+        def get_focus_point():
+            board = SceneUtils.get_object("Darts Board")
+            board_pos = board.location
+            board_r = (
+                SceneUtils.get_geometry_nodes(darts_board)
+                .interface.items_tree.get("Board Diameter")
+                .default_value
+                / 2
+            )
+
+            focus_pos = board_pos.copy()
+            focus_pos[0] += np.random.normal(0, board_r / 3)
+            focus_pos[1] += np.random.normal(0, 0.02)
+            focus_pos[2] += np.random.normal(0, board_r / 3)
+
+            SceneUtils.get_object("Camera Focus").location = focus_pos
+
+            return focus_pos
+
+        focus_point = get_focus_point()
+
+        # -------------------------------------------
+        # Rotation
+        def get_cam_rotation(cam_pos, focus_point):
+            view_dir = focus_point - cam_pos
+            rot_quat = view_dir.to_track_quat("-Z", "Y")
+            return rot_quat.to_euler()
+
+        cam_rot = get_cam_rotation(cam_pos, focus_point)
+        cam.rotation_euler = cam_rot
+
+
+class MaskRendering:
 
     def prepare_objects() -> dict:
         state = {}
@@ -292,7 +296,9 @@ def render_masks(mask_obj_names: str | list[str]):
         for obj in bpy.data.objects:
             obj.hide_render = state[obj.name]
 
-    def set_env_texture(value: float = 0) -> float:
+    def prepare_env_texture() -> dict:
+        state = {}
+
         # Get node tree
         node_tree = bpy.context.scene.world.node_tree
         if not node_tree:
@@ -310,10 +316,30 @@ def render_masks(mask_obj_names: str | list[str]):
             )
 
         # Set state
-        env_state = bg_node.inputs["Strength"].default_value
+        state["default_value"] = bg_node.inputs["Strength"].default_value
         bg_node.inputs["Strength"].default_value = 0
 
-        return env_state
+        return state
+
+    def restore_env_texture(state: dict):
+        # Get node tree
+        node_tree = bpy.context.scene.world.node_tree
+        if not node_tree:
+            raise RuntimeError(
+                "No world node tree containing environment texture to clear."
+            )
+
+        # Get Background node
+        for bg_node in node_tree.nodes:
+            if bg_node.type == "BACKGROUND":
+                break
+        else:
+            raise RuntimeError(
+                "No background node in world node tree. Could not remove environment texture."
+            )
+
+        # Set state
+        bg_node.inputs["Strength"].default_value = state["default_value"]
 
     def prepare_compositor() -> dict:
         state = {}
@@ -391,58 +417,60 @@ def render_masks(mask_obj_names: str | list[str]):
         scene.render.filepath = state["filepath"]
         bpy.context.scene.render.engine = state["engine"]
 
-    if type(mask_obj_names) == str:
-        mask_obj_names = [mask_obj_names]
+    def render_masks(mask_obj_names: str | list[str]):
 
-    # Prepare state
-    obj_state = prepare_objects()
-    bpy.context.scene.render.film_transparent = True
-    env_state = set_env_texture()
-    compositor_state = prepare_compositor()
-    settings_state = prepare_settings()
+        if type(mask_obj_names) == str:
+            mask_obj_names = [mask_obj_names]
 
-    # Render the scene
-    for mask_obj_name in mask_obj_names:
-        # Prepare mask object
-        mask_obj = get_object(mask_obj_name)
-        mask_obj.hide_render = False
+        # Prepare state
+        obj_state = MaskRendering.prepare_objects()
+        bpy.context.scene.render.film_transparent = True
+        env_state = MaskRendering.prepare_env_texture()
+        compositor_state = MaskRendering.prepare_compositor()
+        settings_state = MaskRendering.prepare_settings()
 
-        # Render
-        out_name = f"mask_{mask_obj_name.replace(' ', '_')}.png"
-        bpy.context.scene.render.filepath = os.path.join("dump", out_name)
-        bpy.ops.render.render(write_still=True)
+        # Render the scene
+        for mask_obj_name in mask_obj_names:
+            # Prepare mask object
+            mask_obj = SceneUtils.get_object(mask_obj_name)
+            mask_obj.hide_render = False
 
-        # Restore mask object
-        mask_obj.hide_render = True
+            # Render
+            out_name = f"mask_{mask_obj_name.replace(' ', '_')}.png"
+            bpy.context.scene.render.filepath = os.path.join("dump", out_name)
+            bpy.ops.render.render(write_still=True)
 
-    # Restore state
-    restore_settings(settings_state)
-    restore_compositor(compositor_state)
-    set_env_texture(env_state)
-    bpy.context.scene.render.film_transparent = False
-    restore_objects(obj_state)
+            # Restore mask object
+            mask_obj.hide_render = True
+
+        # Restore state
+        MaskRendering.restore_settings(settings_state)
+        MaskRendering.restore_compositor(compositor_state)
+        MaskRendering.restore_env_texture(env_state)
+        bpy.context.scene.render.film_transparent = False
+        MaskRendering.restore_objects(obj_state)
 
 
 # Get Scene Infos
-darts_board = get_object("Darts Board")
-db_geonodes = get_geometry_nodes(darts_board)
+darts_board = SceneUtils.get_object("Darts Board")
+db_geonodes = SceneUtils.get_geometry_nodes(darts_board)
 
 board_radius = db_geonodes.interface.items_tree.get("Radius 6").default_value
 board_center = darts_board.location
 
 # Place Darts
-darts_placement()
-scores, total_score = calculate_dart_score()
+ObjectPlacement.place_darts()
+scores, total_score = SceneUtils.calculate_dart_score()
 
 # Place Camera
-place_camera()
+ObjectPlacement.place_camera()
 
 # Randomize HDRI
 random_env_texture()
 
 # Render
-bpy.ops.render.render(write_still=True)
-render_masks(["Darts Board Area", "Dart 1", "Dart 2", "Dart 3"])
+# bpy.ops.render.render(write_still=True)
+MaskRendering.render_masks(["Darts Board Area", "Dart 1", "Dart 2", "Dart 3"])
 
 print(scores, total_score)
 
