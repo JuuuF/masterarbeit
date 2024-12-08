6645374d2b50ae394ab534d14448659917891470
Justin <juswerth@gmx.de>
2024-12-08 18:04:12 +0100

    first steps towards python package

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="ma_darts",
+    version="0.1.0",
+    description="Master Thesis darts scoring system",
+    author="JuuuF",
+    packages=find_packages(where="src"),
+    package_dir={"": "src"},
+    python_requires=">=3.11.5",
+    install_requires=[
+        # TODO
+    ],
+)
