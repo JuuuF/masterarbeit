fb7569a9c96051996e13b79767b6df60c2560ea8
JuuuF <juswerth@gmx.de>
2024-12-21 23:24:26 +0100

    more robust angle alignment

--- src/ma_darts/cv/cv.py
+++ src/ma_darts/cv/cv.py
@@ -1673,6 +1673,7 @@ class CV:
         cy: int,
         cx: int,
         lines: list[tuple[float, float]],
+        show: bool = False
     ):
 
         def theta_change_shear_y(theta, shear_y):
@@ -1714,6 +1715,7 @@ class CV:
             return M_shear
 
         def visualize_matrix(M):
+            nonlocal src_start
             global cy, cx, img
             res = img.copy()
             for t in src_start:
@@ -1729,171 +1731,186 @@ class CV:
                 )
             return res
 
-        # Translate center to (0, 0)
-        M_trans_a = np.array(
-            [
-                [1, 0, -cx],
-                [0, 1, -cy],
-                [0, 0, 1],
-            ]
-        )
-        M_trans_b = np.array(
-            [
-                [1, 0, cx],
-                [0, 1, cy],
-                [0, 0, 1],
-            ]
-        )
-
         angle_step = np.pi / 10
-        dst = np.arange(0, np.pi, angle_step) + angle_step / 2
-        src = np.array([l[1] for l in lines])
-        # copy for drawing
-        src_start = src.copy()
+        dst_start = np.arange(0, np.pi, angle_step) + angle_step / 2
+        src_start = np.array([l[1] for l in lines])
 
-        # Initialize Matrix
-        M = np.eye(3)
+        transformation_matrices = []
 
-        # -----------------------------
-        # 1. Translate center to origin
+        for start_line in range(10):
 
-        # update transformation matrix
-        M = M_trans_a @ M
+            # Initialize Values
+            M = np.eye(3)
+            src = src_start.copy()
+            dst = dst_start.copy()
 
-        # -----------------------------
-        # 2. Align src vertival line with destination vertical line
-        t_src = src[0]
-        t_dst = dst[0]
-        rot_angle = t_src - t_dst
-        M_rot = rotation_matrix(rot_angle)
-
-        # update transformation matrix
-        M = M_rot @ M
-
-        # update src points
-        src = src - rot_angle
-
-        # draw
-        res = visualize_matrix(M_trans_b @ M)
-        for t in src:
-            draw_polar_line_through_point(
-                res, (int(cy), int(cx)), t, thickness=2, color=(0, 0, 0)
+            # -----------------------------
+            # 1. Translate center to origin
+            M_trans_a = np.array(
+                [
+                    [1, 0, -cx],
+                    [0, 1, -cy],
+                    [0, 0, 1],
+                ]
             )
-        show_imgs(align_lines=res, block=False)
-
-        # -----------------------------
-        # 3. Vertical alignment
-        M_rot_v = rotation_matrix(angle_step / 2)
-
-        # update transformation matrix
-        M = M_rot_v @ M
-
-        # update src and dst points
-        src -= angle_step / 2
-        dst -= angle_step / 2
-
-        # draw
-        res = visualize_matrix(M_trans_b @ M)
-        for t in src:
-            draw_polar_line_through_point(
-                res, (int(cy), int(cx)), t, thickness=2, color=(0, 0, 0)
+            M_trans_b = np.array(
+                [
+                    [1, 0, cx],
+                    [0, 1, cy],
+                    [0, 0, 1],
+                ]
             )
-        show_imgs(vertical_alignment=res, block=False)
 
-        # -----------------------------
-        # 4. Vertical shearing
-        t_src = src[5]
-        t_dst = dst[5]
-        shear_amount = t_dst - t_src
+            # update transformation matrix
+            M = M_trans_a @ M
 
-        M_shear = np.array(
-            [
-                [1, 0, 0],
-                [shear_amount, 1, 0],
-                [0, 0, 1],
-            ]
-        )
+            # -----------------------------
+            # 2. Align src vertival line with destination vertical line
+            t_src = src[start_line]
+            t_dst = dst[start_line]
+            rot_angle = t_src - t_dst
+            M_rot = rotation_matrix(rot_angle)
 
-        # update transformation matrix
-        M = M_shear @ M
+            # update transformation matrix
+            M = M_rot @ M
 
-        # update src points
-        src = theta_change_shear_y(src, shear_amount)
-        src[src < 0] += np.pi
+            # update src points
+            src = src - rot_angle
 
-        # draw
-        res = visualize_matrix(M_trans_b @ M)
-        for t in src:
-            draw_polar_line_through_point(
-                res, (int(cy), int(cx)), t, thickness=2, color=(0, 0, 0)
+            # draw
+            if show:
+                res = visualize_matrix(M_trans_b @ M)
+                for t in src:
+                    draw_polar_line_through_point(
+                        res, (int(cy), int(cx)), t, thickness=2, color=(0, 0, 0)
+                    )
+                show_imgs(align_lines=res, block=False)
+
+            # -----------------------------
+            # 3. Vertical alignment
+            rotation_alignment_angle = start_line * angle_step + angle_step / 2
+            M_rot_v = rotation_matrix(rotation_alignment_angle)
+
+            # update transformation matrix
+            M = M_rot_v @ M
+
+            # update src and dst points
+            src -= rotation_alignment_angle
+            dst -= rotation_alignment_angle
+
+            # draw
+            if show:
+                res = visualize_matrix(M_trans_b @ M)
+                for t in src:
+                    draw_polar_line_through_point(
+                        res, (int(cy), int(cx)), t, thickness=2, color=(0, 0, 0)
+                    )
+                show_imgs(vertical_alignment=res, block=False)
+
+            # -----------------------------
+            # 4. Vertical shearing
+            horizontal_line_idx = (start_line + 5) % len(lines)
+            t_src = src[horizontal_line_idx]
+            t_dst = dst[horizontal_line_idx]
+            shear_amount = t_dst - t_src
+
+            M_shear = np.array(
+                [
+                    [1, 0, 0],
+                    [shear_amount, 1, 0],
+                    [0, 0, 1],
+                ]
             )
-        show_imgs(vertical_shearing=res, block=False)
 
-        # -----------------------------
-        # 5. Vertical scaling
+            # update transformation matrix
+            M = M_shear @ M
 
-        # convert angles to slopes
-        slopes_src = np.tan(src)
-        slopes_dst = np.tan(dst)
-        # remove already aligned angles
-        slopes_src = np.delete(slopes_src, [0, 5])
-        slopes_dst = np.delete(slopes_dst, [0, 5])
-        # calculate scaling
-        scales = slopes_src / slopes_dst
-        scale = np.mean(scales)
+            # update src points
+            src = theta_change_shear_y(src, shear_amount)
+            src[src < 0] += np.pi
 
-        M_scale = np.array(
-            [
-                [1, 0, 0],
-                [0, scale, 0],
-                [0, 0, 1],
-            ]
-        )
+            # draw
+            if show:
+                res = visualize_matrix(M_trans_b @ M)
+                for t in src:
+                    draw_polar_line_through_point(
+                        res, (int(cy), int(cx)), t, thickness=2, color=(0, 0, 0)
+                    )
+                show_imgs(vertical_shearing=res, block=False)
 
-        # update transformation matrix
-        M = M_scale @ M
+            # -----------------------------
+            # 5. Vertical scaling
 
-        # upate src points
-        src = np.arctan(np.tan(src) / scale)
+            # convert angles to slopes
+            slopes_src = np.tan(src)
+            slopes_dst = np.tan(dst)
+            # remove already aligned angles
+            slopes_src = np.delete(slopes_src, [start_line, horizontal_line_idx])
+            slopes_dst = np.delete(slopes_dst, [start_line, horizontal_line_idx])
+            # calculate scaling
+            scales = slopes_src / slopes_dst
+            scale = np.mean(scales)
 
-        # draw
-        res = visualize_matrix(M_trans_b @ M)
-        for j, t in enumerate(src):
-            draw_polar_line_through_point(
-                res, (int(cy), int(cx)), t, thickness=2, color=(0, 0, 0)
+            M_scale = np.array(
+                [
+                    [1, 0, 0],
+                    [0, scale, 0],
+                    [0, 0, 1],
+                ]
             )
-        show_imgs(vertical_scaling=res, block=False)
 
-        # -----------------------------
-        # 6. Undo vertical alignment
-        M_rot_v_inv = rotation_matrix(-angle_step / 2)
+            # update transformation matrix
+            M = M_scale @ M
 
-        # update transformation matrix
-        M = M_rot_v_inv @ M
+            # upate src points
+            src = np.arctan(np.tan(src) / scale)
 
-        # update src and dst points
-        src += angle_step / 2
-        dst += angle_step / 2
+            # draw
+            if show:
+                res = visualize_matrix(M_trans_b @ M)
+                for j, t in enumerate(src):
+                    draw_polar_line_through_point(
+                        res, (int(cy), int(cx)), t, thickness=2, color=(0, 0, 0)
+                    )
+                show_imgs(vertical_scaling=res, block=False)
 
-        # draw
-        res = visualize_matrix(M_trans_b @ M)
-        for j, t in enumerate(src):
-            draw_polar_line_through_point(
-                res, (int(cy), int(cx)), t, thickness=2, color=(0, 0, 0)
-            )
-        show_imgs(vertical_scaling=res, block=False)
+            # -----------------------------
+            # 6. Undo vertical alignment
+            M_rot_v_inv = rotation_matrix(-rotation_alignment_angle)
 
-        # -----------------------------
-        # 6. Re-Translate into center
+            # update transformation matrix
+            M = M_rot_v_inv @ M
 
-        M = M_trans_b @ M
+            # update src and dst points
+            src += rotation_alignment_angle
+            dst += rotation_alignment_angle
+
+            # draw
+            if show:
+                res = visualize_matrix(M_trans_b @ M)
+                for j, t in enumerate(src):
+                    draw_polar_line_through_point(
+                        res, (int(cy), int(cx)), t, thickness=2, color=(0, 0, 0)
+                    )
+                show_imgs(vertical_scaling=res, block=False)
+
+            # -----------------------------
+            # 7. Re-Translate into center
+
+            M = M_trans_b @ M
+
+            transformation_matrices.append(M)
 
         # - # - # - # - # - # - # - # - # - #
 
-        global img
-        img = cv2.warpPerspective(img, M, (img.shape[1], img.shape[0]))
-        show_imgs(img, block=False)
+        M = np.mean(transformation_matrices, axis=0)
 
+        if show:
+            global img
+            res = cv2.warpPerspective(img.copy(), M, (img.shape[1], img.shape[0]))
+            show_imgs(img_undistort=res, block=False)
+
+        return M
 
 def extract_center(img: np.ndarray):
     edges = CV.edge_detect(img)
@@ -1951,7 +1968,11 @@ if __name__ == "__main__":
 
         cy, cx = CV.center_point_from_lines(lines)
 
-        CV.undistort_by_lines(cy, cx, lines)
+        M_undistort = CV.undistort_by_lines(cy, cx, lines, show=False)
+
+        # TODO: correct output size
+        img_undistort = cv2.warpPerspective(img.copy(), M_undistort, (img.shape[1], img.shape[0]))
+        show_imgs(img, img_undistort)
 
         show_imgs()
         continue
