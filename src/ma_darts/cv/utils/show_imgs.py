87f30dca801b1f1b753e050f7ea9ef7e29fc1fdd
JuuuF <juswerth@gmx.de>
2024-12-22 19:36:42 +0100

    updated show_imgs function to save displayed images into dump directory

--- src/ma_darts/cv/utils/show_imgs.py
+++ src/ma_darts/cv/utils/show_imgs.py
@@ -1,5 +1,20 @@
+import os
 import cv2
 import numpy as np
+
+
+def _display_and_save(
+    name: str,
+    img: np.ndarray,
+    out_dir: str = "dump/display_out",
+) -> None:
+    # Show image
+    cv2.imshow(name, img)
+
+    # Save image
+    if not os.path.exists(out_dir):
+        os.makedirs(out_dir)
+    cv2.imwrite(os.path.join(out_dir, name + ".png"), img)
 
 
 def show_imgs(
@@ -8,9 +23,9 @@ def show_imgs(
     **named_imgs: dict[str, np.ndarray],
 ) -> None:
     for i, img in enumerate(imgs):
-        cv2.imshow(f"img_{i}", img)
+        _display_and_save(f"img_{i}", img)
     for name, img in named_imgs.items():
-        cv2.imshow(name, img)
+        _display_and_save(name, img)
 
     if not block:
         return
