940638e70de283549793673f0a84944f390dc287
Justin <juswerth@gmx.de>
2025-02-14 09:30:20 +0100

    better training outputs watcher

--- remote/watch_training.sh
+++ remote/watch_training.sh
@@ -1,7 +1,9 @@
 #1/bin/sh
 
 while true; do
-    scp ma_kiel:/home/jfuerstenwerth/masterarbeit/dump/pred.png dump/.
-    scp ma_kiel:/home/jfuerstenwerth/masterarbeit/dump/training_history.png dump/.
+    ssh ma_kiel cp /home/jfuerstenwerth/masterarbeit/dump/pred.png /home/jfuerstenwerth/masterarbeit/dump/transfer.png
+    scp ma_kiel:/home/jfuerstenwerth/masterarbeit/dump/transfer.png dump/pred.png
+    ssh ma_kiel cp /home/jfuerstenwerth/masterarbeit/dump/training_history.png /home/jfuerstenwerth/masterarbeit/dump/transfer.png
+    scp ma_kiel:/home/jfuerstenwerth/masterarbeit/dump/transfer.png dump/training_history.png
     sleep 60
 done
