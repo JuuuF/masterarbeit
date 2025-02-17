03e97242874655c49f7db70d09620337e93961b2
Justin <juswerth@gmx.de>
2025-02-17 12:51:22 +0100

    added global darts variables

--- src/ma_darts/__init__.py
+++ src/ma_darts/__init__.py
@@ -1,5 +1,8 @@
 dart_order = [20, 1, 18, 4, 13, 6, 10, 15, 2, 17, 3, 19, 7, 16, 8, 11, 14, 9, 12, 5]
+img_size = 800
+classes = ["nothing", "black", "white", "red", "green", "out"]
 
 __all__ = []
-
 __all__.append("dart_order")
+__all__.append("img_size")
+__all__.append("classes")
