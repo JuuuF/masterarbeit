8bfcd17f88590e35f507e92fe48c3492080d1fe0
Justin <juswerth@gmx.de>
2025-02-17 12:51:45 +0100

    more boilerplate augmentation code

--- src/ma_darts/ai/data/augmentation.py
+++ src/ma_darts/ai/data/augmentation.py
@@ -5,10 +5,26 @@ class Augmentation:
     def __init__(self):
         pass
 
+    def pixel_augmentation(
+        self,
+        img: tf.Tensor,  # (800, 800, 3)
+    ):
+        return img
+
+    def transformation_augmentation(
+        self,
+        img: tf.Tensor,  # (800, 800, 3)
+        pos: tf.Tensor,  # (2, 3)
+    ):
+        return img, pos
+
     def __call__(
         self,
         img: tf.Tensor,  # (800, 800, 3)
         pos: tf.Tensor,  # (2, 3)
         cls: tf.Tensor,  # (6, 3)
-    ):
+    ) -> tuple[tf.Tensor, tf.Tensor, tf.Tensor]:
+        img = self.pixel_augmentation(img)
+
+        img, pos = self.transformation_augmentation(img, pos)
         return img, pos, cls
