2820e976b5bff03c6c2f5ae42944a22d09e119a4
Justin <juswerth@gmx.de>
2025-02-24 01:24:38 +0100

    added existence computation util function - this time correctly

--- src/ma_darts/ai/utils/tensors.py
+++ src/ma_darts/ai/utils/tensors.py
@@ -1,7 +1,7 @@
 import tensorflow as tf
 
 
-def get_grid_existence(
+def get_grid_existence_per_cell(
     t: tf.Tensor,  # (bs, s, s, 8, 3)
 ) -> tf.Tensor:  # (bs, s, s)
     shape = tf.shape(t)  # bs, s, s, 8, 3
@@ -20,3 +20,23 @@ def get_grid_existence(
     xst_prob = something / denom
 
     return xst_prob  # (bs, s, s)
+
+
+def get_grid_existences(
+    t: tf.Tensor,  # (bs, s, s, 8, 3)
+) -> tf.Tensor:  # (bs, s, s, 1, 3)
+    shape = tf.shape(t)  # bs, s, s, 8, 3
+
+    # Remove positions
+    t = t[..., 2:, :]  # (bs, s, s, 6, 3)
+
+    # Find existences
+    nothing = t[..., 0, :]  # (bs, s, s, 3)
+    something = tf.reduce_sum(t[..., 1:, :], axis=-2)  # (bs, s, s, 3)
+
+    denom = tf.maximum(something + nothing, 1e-6)
+    xst_prob = something / denom
+
+    xst_prob = tf.expand_dims(xst_prob, axis=-1)  # (bs, s, s, 1, 3)
+
+    return xst_prob  # (bs, s, s, 1, 3)
