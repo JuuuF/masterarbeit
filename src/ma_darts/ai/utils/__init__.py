561d7193fa52e8e402b9f699dc5869cf52aa63d6
Justin <juswerth@gmx.de>
2025-02-24 01:25:06 +0100

    also adding the functions to the module

--- src/ma_darts/ai/utils/__init__.py
+++ src/ma_darts/ai/utils/__init__.py
@@ -2,8 +2,10 @@ __all__ = []
 
 from .scoring import get_dart_scores
 from .scoring import get_absolute_score_error
-from .tensors import get_grid_existence
+from .tensors import get_grid_existence_per_cell
+from .tensors import get_grid_existences
 
 __all__.append("get_dart_scores")
 __all__.append("get_absolute_score_error")
-__all__.append("get_grid_existence")
+__all__.append("get_grid_existence_per_cell")
+__all__.append("get_grid_existences")
