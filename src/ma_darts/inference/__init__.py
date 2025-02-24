ca7516c38da52287727bb5a44bc5f2d5fb6ef256
Justin <juswerth@gmx.de>
2025-02-24 18:30:57 +0100

    systems inference starting point

---
+++ src/ma_darts/inference/__init__.py
@@ -0,0 +1,7 @@
+__all__ = []
+
+from .ma import inference_ma
+from .deepdarts import inference_deepdarts
+
+__all__.append("inference_ma")
+__all__.append("inference_deepdarts")
---
+++ src/ma_darts/inference/deepdarts.py
@@ -0,0 +1,10 @@
+import numpy as np
+
+
+def inference_deepdarts(imgs: list[np.ndarray] | np.ndarray) -> list[np.ndarray]:
+    if type(imgs) != list:
+        imgs = [imgs]
+
+    # TODO: inference
+
+    return imgs
