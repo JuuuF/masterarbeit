04b5d3eaf79eecbd5b6c0b91a597dda9383f2579
Justin <juswerth@gmx.de>
2025-02-24 17:14:40 +0100

    slightly improving cell existence calculation

--- src/ma_darts/ai/utils/tensors.py
+++ src/ma_darts/ai/utils/tensors.py
@@ -10,14 +10,13 @@ def get_grid_existence_per_cell(
     t = t[..., 2:, :]  # (bs, s, s, 6, 3)
 
     # Concat entries per cell
-    t = tf.reduce_sum(t, axis=-1) / 3  # (bs, s, s, 6)
+    t = tf.reduce_sum(t, axis=-1)  # (bs, s, s, 6)
 
     # Find existences
     nothing = t[..., 0]  # (bs, s, s)
     something = tf.reduce_sum(t[..., 1:], axis=-1)  # (bs, s, s)
 
-    denom = tf.maximum(something + nothing, 1e-6)
-    xst_prob = something / denom
+    xst_prob = tf.math.divide_no_nan(something, something + nothing)
 
     return xst_prob  # (bs, s, s)
 
