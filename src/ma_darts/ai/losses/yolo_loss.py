06a9f4eb55f8b6e33d70fa4eeb44590638f68710
Justin <juswerth@gmx.de>
2025-02-26 00:42:48 +0100

    implemented new losses into yolo loss

--- src/ma_darts/ai/losses/yolo_loss.py
+++ src/ma_darts/ai/losses/yolo_loss.py
@@ -2,7 +2,7 @@ import numpy as np
 import tensorflow as tf
 
 from ma_darts import img_size
-from ma_darts.ai.losses import ExistenceLoss, ClassesLoss, PositionsLoss
+from ma_darts.ai.losses import ExistenceLoss, ClassesLoss, PositionsLoss, DIoULoss
 
 
 class YOLOv8Loss(tf.keras.losses.Loss):
@@ -11,33 +11,49 @@ class YOLOv8Loss(tf.keras.losses.Loss):
         square_size: float = 0.02,
         class_introduction_threshold: float = np.inf,
         position_introduction_threshold: float = np.inf,
+        diou_introduction_threshold: float = np.inf,
         *args,
         **kwargs
     ):
         super(YOLOv8Loss, self).__init__(*args, **kwargs)
         self.square_size = square_size
-        self.class_introduction_threshold = class_introduction_threshold
-        self.position_introduction_threshold = position_introduction_threshold
 
+        # Existence Loss
         self.xst_loss = ExistenceLoss()
-        self.cls_loss = ClassesLoss()
-        self.pos_loss = PositionsLoss(self.square_size)
 
+        # Classes Loss
+        self.cls_loss = ClassesLoss()
+        self.base_cls_loss = tf.constant(2, tf.float32)
+        self.class_introduction_threshold = class_introduction_threshold
         self.cls_introduction_end = tf.constant(
             class_introduction_threshold, tf.float32
         )
         self.cls_introduction_start = tf.constant(
             2 * class_introduction_threshold, tf.float32
         )
-        self.base_cls_loss = tf.constant(2, tf.float32)
 
+        # Positions Loss
+        self.pos_loss = PositionsLoss()
+        self.base_pos_loss = tf.constant(2, tf.float32)
+        self.position_introduction_threshold = position_introduction_threshold
         self.pos_introduction_end = tf.constant(
             position_introduction_threshold, tf.float32
         )
         self.pos_introduction_start = tf.constant(
             2 * position_introduction_threshold, tf.float32
         )
-        self.base_pos_loss = tf.constant(2, tf.float32)
+
+        # DIoU Loss
+        self.diou_loss = DIoULoss(self.square_size)
+        self.base_diou_loss = tf.constant(0.1, tf.float32)
+        self.diou_introduction_threshold = diou_introduction_threshold
+        self.diou_introduction_end = tf.constant(
+            diou_introduction_threshold, tf.float32
+        )
+        self.diou_introduction_start = tf.constant(
+            2 * diou_introduction_threshold, tf.float32
+        )
+        self.diou_multiplier = tf.constant(0.2)
 
     def get_factor(
         self,
@@ -99,6 +115,27 @@ class YOLOv8Loss(tf.keras.losses.Loss):
         )
         return loss
 
+    def get_diou_loss(
+        self,
+        y_true: tf.Tensor,  # (bs, s, s, 8, 3)
+        y_pred: tf.Tensor,
+        total_loss: tf.Tensor,  # ()
+    ) -> tf.Tensor:  # ()
+
+        diou_fac = self.get_factor(
+            self.diou_introduction_start,
+            self.diou_introduction_end,
+            total_loss,
+        )
+
+        loss = tf.where(
+            tf.equal(diou_fac, 0.0),
+            self.base_diou_loss,
+            pos_fac * self.diou_loss(y_true, y_pred)
+            + (1 - pos_fac) * self.base_diou_loss,
+        )
+        return loss
+
     def call(self, y_true, y_pred):
         xst_loss = self.xst_loss(y_true, y_pred)
         total_loss = xst_loss
@@ -117,6 +154,10 @@ class YOLOv8Loss(tf.keras.losses.Loss):
         # print("pos_loss", pos_loss.numpy())
         # print("total_loss", total_loss.numpy())
         # print()
+
+        # diou_loss = self.get_diou_loss(y_true, y_pred, total_loss)
+        # diou_loss = diou_loss * self.diou_multiplier
+        # total_loss = total_loss + diou_loss
 
         return total_loss
 
@@ -137,7 +178,7 @@ if __name__ == "__main__":
     X, y_true = pickle.load(open("dump/sample.pkl", "rb"))
     y_pred = [tf.gather(y, [3, 2, 1, 0], axis=0) for y in y_true]
     y_pred = [
-        tf.clip_by_value(y + tf.random.normal(y.shape, stddev=0.02), 0, 1)
+        tf.clip_by_value(y + tf.random.normal(y.shape, stddev=0.1, seed=0), 0, 1)
         for y in y_pred
     ]
 
