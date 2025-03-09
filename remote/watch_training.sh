2de01a6bcb60a7924287166a4cd3dfcdc2becfb6
Justin <juswerth@gmx.de>
2025-03-09 22:42:44 +0100

    better training watching

--- remote/watch_training.sh
+++ remote/watch_training.sh
@@ -1,9 +1,19 @@
 #1/bin/sh
 
+prev_hash=""
+
 while true; do
-    # ssh ma_kiel cp /home/jfuerstenwerth/masterarbeit/dump/pred.png /home/jfuerstenwerth/masterarbeit/dump/transfer.png
-    # scp ma_kiel:/home/jfuerstenwerth/masterarbeit/dump/transfer.png dump/pred.png
-    ssh ma_kiel cp /home/jfuerstenwerth/masterarbeit/dump/training_history.png /home/jfuerstenwerth/masterarbeit/dump/transfer.png
-    scp ma_kiel:/home/jfuerstenwerth/masterarbeit/dump/transfer.png dump/training_history.png
-    sleep 20
+    new_hash=$(ssh ma_kiel md5sum masterarbeit/dump/training_history.png | awk '{print $1}')
+
+    if [ "$prev_hash" != "$new_hash" ]; then
+        scp ma_kiel:/home/jfuerstenwerth/masterarbeit/dump/training_history.png dump/training_history.png
+        scp ma_kiel:/home/jfuerstenwerth/masterarbeit/dump/training_history.pkl dump/training_history.pkl
+
+        # Find latest checkpoint
+        latest_dir=$(ssh ma_kiel "ls -1t masterarbeit/data/ai/checkpoints/darts | head -n 1")
+        rsync -avz --progress ma_kiel:masterarbeit/data/ai/checkpoints/darts/$latest_dir/latest.weights.h5 data/ai/darts/
+
+        prev_hash="$new_hash"
+    fi
+    sleep 2
 done
