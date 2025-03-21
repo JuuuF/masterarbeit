abab762854b0e8e8b2b3e99c7fb59e6a5bc1d05a
Justin <juswerth@gmx.de>
2025-03-21 11:52:36 +0100

    new chapters and new drawio diagrams

--- writing/work/imgs/export_diagrams.py
+++ writing/work/imgs/export_diagrams.py
@@ -1,36 +1,17 @@
 import os
 from subprocess import run
 
-imgs_dir = "writing/work/imgs"
-diagram_mappings = {
-    "Project": os.path.join(imgs_dir, "ma_project_structure.pdf"),
-    "|| Data ||": os.path.join(imgs_dir, "rendering", "rendering_pipeline.pdf"),
-    "Data - Heatmaps": os.path.join(imgs_dir, "rendering", "methodik", "heatmaps.pdf"),
-    "|| CV  ||": os.path.join(imgs_dir, "cv", "cv_pipeline.pdf"),
-    "CV - Edges": os.path.join(imgs_dir, "cv", "methodik", "edges.pdf"),
-    "CV - Lines": os.path.join(imgs_dir, "cv", "methodik", "lines.pdf"),
-    "CV - Orientation": os.path.join(imgs_dir, "cv", "methodik", "orientation.pdf"),
-    # "|| AI ||": os.path.join(imgs_dir, ".pdf"),
-    "AI - augmentation": os.path.join(imgs_dir, "ai", "augmentation_pipeline.pdf"),
-}
 
-drawio_file = "writing/work/imgs/charts.drawio"
+imgs_dir = "writing/work/imgs/"
+files = [
+    os.path.join(dp, f)
+    for dp, dn, filenames in os.walk(imgs_dir)
+    for f in filenames
+    if os.path.splitext(f)[1] == ".drawio"
+]
 
-with open(drawio_file, "r") as f:
-    contents = f.read()
-
-pages = contents.split('<diagram id="')[1:]
-pages = [p.split('name="')[1] for p in pages]
-pages = [p.split('">')[0] for p in pages]
-
-for i, page_name in enumerate(pages):
-    output_file = diagram_mappings.get(page_name, None)
-    if output_file is None:
-        print(
-            f"WARNING: no output file specified for diagram page {page_name}. Skipping."
-        )
-        continue
-
+for file in files:
+    output_file = file.replace(".drawio", ".pdf")
     run(
         [
             "sh",
@@ -39,15 +20,13 @@ for i, page_name in enumerate(pages):
             " --export"
             " --output"
             f" {output_file}"
-            f" {drawio_file}"
+            f" {file}"
             " --format"
             " pdf"
             " --transparent"
             " --crop"
             " --border"
-            " 0"
-            " --page-index"
-            f" {i}",
+            " 0",
         ],
         check=True,
     )
