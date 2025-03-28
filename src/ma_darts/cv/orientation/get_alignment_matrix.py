c1b4d31137619c3c3596b03960b462f2dfc1462c
--local <juswerth@gmx.de>
2025-03-28 17:07:37 +0100

    commit to proper ransac implementation

--- src/ma_darts/cv/orientation/get_alignment_matrix.py
+++ src/ma_darts/cv/orientation/get_alignment_matrix.py
@@ -3,9 +3,15 @@ import numpy as np
 from ma_darts.cv.utils import points_transformation, show_imgs
 
 
-def alternative_ransac(
-    src_pts, dst_pts, cy, cx, inlier_distance_threshold: float = 100.0
+def get_alignment_matrix(
+    src_pts: list[tuple[float, float]],
+    dst_pts: list[tuple[float, float]],
+    cy: int,
+    cx: int,
 ):
+    # If there are not enough points, we return
+    if len(src_pts) < 4:
+        return np.eye(3)
 
     n_tries = len(src_pts) * 3
     use_top_n = 1
@@ -66,56 +72,3 @@ def alternative_ransac(
     # final_dists = np.linalg.norm(src_pts_transformed - dst_pts, axis=1, ord=2).sum()
     # print(final_dists)
     return M_final
-
-
-def get_alignment_matrix(
-    src_pts: list[tuple[float, float]],
-    dst_pts: list[tuple[float, float]],
-    cy: int,
-    cx: int,
-) -> np.ndarray:
-    # If there are not enough points, we return
-    if len(src_pts) < 4:
-        return np.eye(3)
-
-    M = alternative_ransac(src_pts, dst_pts, cy, cx)
-    return M
-
-    # In case we still have some outliers, we randomly sample 75% of the src and dst points
-    # and find a homography between these point sets. In the end, we take the mean of all homographies
-    n_tries = len(src_pts)
-    min_points = min(10, len(src_pts))
-    Ms = []
-    src_pts = np.float32(src_pts)
-    dst_pts = np.float32(dst_pts)
-    src_center = [cx, cy]
-    dst_center = [400, 400]
-
-    for i in range(n_tries):
-        # Randomly select points
-        indices = np.random.choice(len(src_pts), min_points, replace=False)
-        src_sample = src_pts[indices]
-        dst_sample = dst_pts[indices]
-
-        # Add mid point
-        src_sample = np.insert(src_sample, 0, src_center, axis=0)
-        dst_sample = np.insert(dst_sample, 0, dst_center, axis=0)
-
-        M, _ = cv2.findHomography(
-            src_sample,
-            dst_sample,
-            method=cv2.RANSAC,
-        )
-        if M is not None:
-            Ms.append(M)
-
-    if len(Ms) == 0:
-        print("ERROR: Could not find alignment homography.")
-        return np.eye(3)
-    M = np.median(Ms, axis=0)
-
-    # src_pts_transformed = points_transformation(src_pts[:, ::-1], M)[:, ::-1]
-    # final_dists = np.linalg.norm(src_pts_transformed - dst_pts, axis=1, ord=2).sum()
-    # print(final_dists)
-    # print()
-    return M
